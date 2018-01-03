6ba6466c2e2abd89050712c46ffbe2d36c1ec000
--global <daniel.destouche@gmail.com>
2018-01-03 15:57:24 -0500

    Remove old version

--- index.js
+++ index.js
@@ -1,16 +1,14 @@
 var program = require('commander');
 var MarkdownGenerator = require('./src/markdownGenerator');
 
-// program.arguments('<file>')
-//     .option('-o, -output <outputDir>', 'The directory to output markdown to.', process.cwd())
-//     .action(function(file) {
-//             var generator = new MarkdownGenerator(file, program.outputDir);
-//
-//             generator.emit();
-//     })
-//     .parse(process.argv);
+function runApp(filePath, outputDir) {
+    new MarkdownGenerator(filePath, outputDir)
+        .emit();
+}
 
-
-var generator = new MarkdownGenerator('swagger.json', process.cwd());
-
-generator.emit();
+program.arguments('<file>')
+    .option('-o, -output <outputDir>', 'The directory to output markdown to.', process.cwd())
+    .action(function (file) {
+        runApp(file, program.outputDir);
+    })
+    .parse(process.argv);
